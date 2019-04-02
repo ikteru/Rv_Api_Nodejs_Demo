3c97307daa35fbb32b792eabed575cb4266daa4f
ikteru <t.h.hamza@gmail.com>
2019-04-02 17:31:51 +0100

    reorganized the project a bit and fixed a few complications

---
+++ configs.js
@@ -0,0 +1,12 @@
+
+
+const FILENAME = "cubetest.rvwj";
+const FILE_TO_SLICE =  `./assets/${FILENAME}`;
+const SUPPORT_TYPE = 'n';
+const PRINTER_MODEL = 'IdeaWerk-Speed';
+const CONFIG_PRESET_NAME = 'Recommended';
+const CONFIG_FILE = '';
+
+module.exports = {
+    FILENAME, FILE_TO_SLICE, SUPPORT_TYPE, PRINTER_MODEL, CONFIG_PRESET_NAME, CONFIG_FILE
+};
--- slicer.js
+++ slicer.js
@@ -1,4 +1,3 @@
-
 //Imports //
 let fs = require('fs');
 let request = require('request')
@@ -7,8 +6,7 @@ require("dotenv").config();
 //Global Variables
 const baseUrl = process.env.API_LINK;
 
-
-
+//The HTTP Request functions, all these functions accept 
 module.exports.GetActivationStatus = async function() {
     let formData = {};
     return await module.exports.initializeRequest("POST", baseUrl, "GetActivationStatus", formData);
@@ -33,6 +31,8 @@ module.exports.DownloadFile = async function (id) {
     return await module.exports.initializeRequest("POST", baseUrl, "DownloadFile", formData);
 }
 
+
+//This functions saves the file in the downloads folder after it gets sliced.
 module.exports.saveFile = function (filename, result){
     const fullname = filename + "." + Date.now() + ".fcode";
     const file = fs.createWriteStream(`./downloads/${fullname}`);
@@ -41,6 +41,7 @@ module.exports.saveFile = function (filename, result){
     console.log("File saved successfully in the Downloads folder under the name ::::: ", fullname);
 }
 
+//This is the function you call in order to execute the HTTP requests, all the functions above use this function.
 module.exports.initializeRequest = function (method, baseUrl, serviceCall, formData) {
     // Setting URL and headers for request
     let options = {
@@ -48,13 +49,13 @@ module.exports.initializeRequest = function (method, baseUrl, serviceCall, formD
         url: baseUrl + "/" + serviceCall,
         headers: {
             'Ocp-Apim-Subscription-Key': process.env.SUBSCRIPTION_KEY,
-            //'content-type': 'multipart/form-data'
+            'content-type': 'multipart/form-data'
         },
         formData: formData
     }
     // Return new promise 
     return new Promise(function (resolve, reject) {
-        // Do async job
+        // Make the http request using the lightweight "request" library
         request(options, function (err, resp, body) {
             if (err) {
                 reject(err);
